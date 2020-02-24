75b6d93bbc8224ffc9fb4233fc0d7eedb9e3987a
Oribe Abad <oribe@insaite.io>
2020-02-24 09:44:47 -0600

    change splitter definition

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -1,12 +1,15 @@
 """
-Have the function Splitter(string) read the string, find the separator and return it.
+Have the function Splitter(string) read the string, find the separator return it and the number of strings.
 
 For example:
-abc,def,ghi   -> should return ','
-12ab-cde-123-hij  -> should return '-'
-abcdefghi1234 -> should return None
-""(empty string) -> should return None
+abc,def,ghi   -> should return ',',3
+12ab-cde-123-hij  -> should return '-',4
+abcdefghi1234 -> should return None,0
+""(empty string) -> should return None,0
+
+The strings only can contain letters(a-z or A-Z) and numbers(0-9).
+The separators are special characters as: ",",".","-","_","*","?","#","&","|","+","~" etc.
 """
 
 def Splitter(string:str):
-    return ""
+    return "",0
