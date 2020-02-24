b4e7f00173ab03fae210bbb54c496d82a82603b1
Oribe Abad <oribe@insaite.io>
2020-02-24 09:20:06 -0600

    add splitter challenge

---
+++ splitter/splitter.py
@@ -0,0 +1,12 @@
+"""
+Have the function Splitter(string) read the string, find the separator and return it.
+
+For example:
+abc,def,ghi   -> should return ','
+12ab-cde-123-hij  -> should return '-'
+abcdefghi1234 -> should return None
+""(empty string) -> should return None
+"""
+
+def Splitter(string:str):
+    return ""
